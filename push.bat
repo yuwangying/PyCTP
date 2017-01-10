fb39210603b85ace04e6bcfc7d31fdabbe2207ed
yuwangying <55297268@qq.com>
2017-01-10 15:57:20 +0800

    2017/01/10 15:56:49 backup from windows, comments: CTPManager初始化过程中异常，向界面弹窗，已完成

--- push.bat
+++ push.bat
@@ -1,9 +1,13 @@
+@echo off
 set d=%date:~0,10%
 set t=%time:~0,8%
 set timestamp=%d% %t%
 
+set /p comments=please input commit comments:
+
+git status
 git add .
-git commit -m "%timestamp% backup from windows"
+git commit -m "%timestamp% backup from windows, comments: %comments%"
 git push origin PyCTP_Dev
 echo "Finished Push!"
 pause
