13c183fa54b4a4b3e70355b7f750cedf6dc168b4
yuwangying <55297268@qq.com>
2017-01-10 15:59:48 +0800

    2017/01/10 15:59:15

--- push.bat
+++ push.bat
@@ -3,11 +3,13 @@ set d=%date:~0,10%
 set t=%time:~0,8%
 set timestamp=%d% %t%
 
+git status
+
 set /p comments=please input commit comments:
 
 git status
 git add .
-git commit -m "%timestamp% backup from windows, comments: %comments%"
+git commit -m "%timestamp% %comments%"
 git push origin PyCTP_Dev
 echo "Finished Push!"
 pause
